06a00f75111a419f904ee1f3c40f942c2b405588
Akashdip Mahapatra <81384987+akashdip2001@users.noreply.github.com>
2024-04-04 02:42:34 +0530

    Update onlyADD.c

--- c_project/onlyADD.c
+++ c_project/onlyADD.c
@@ -2,41 +2,45 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
-FILE *fp;
- struct name{
+
+struct record{
     char date[12];
     int id; 
     char name[10];
     unsigned int total;
     int percentage;
-}st;
+};
+
 void add(){
-   
-    fp = fopen("Fileprac.txt","ab");
-    fprintf(fp,"\n\n",1,1);
-    char datt[12];
+    FILE *fp;
+    fp = fopen("Fileprac.txt", "ab");
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    struct record st;
+    char date[12];
     time_t s = time(NULL);
     struct tm dd = *localtime(&s);
-    sprintf(datt,"%02d/%02d/%02d",dd.tm_mday,dd.tm_mon +1,dd.tm_year+1900);
-    strcpy(st.date,datt);
-    printf("enter a key to continue.....");
-    getchar();
-    printf("enter the id: ");
-    scanf("%d",&st.id);
-    getchar();
-    printf("enter the name: ");
-    fflush(stdin);
-    scanf("%s",&st.name);
-    printf("enter the total: ");
-    fflush(stdin);
-    scanf("%u",&st.total);
+    sprintf(date, "%02d/%02d/%02d", dd.tm_mday, dd.tm_mon + 1, dd.tm_year + 1900);
+    strcpy(st.date, date);
+
+    printf("Enter the id: ");
+    scanf("%d", &st.id);
+
+    printf("Enter the name: ");
+    scanf("%9s", st.name);
+
+    printf("Enter the total: ");
+    scanf("%u", &st.total);
     
-    printf("added successfully\n");
-    fwrite(&st, sizeof(st) , 1 ,fp);
+    printf("Added successfully\n");
+    fwrite(&st, sizeof(st), 1, fp);
     fclose(fp);
 }
+
 int main(){
     add();
-    
     return 0;
 }
