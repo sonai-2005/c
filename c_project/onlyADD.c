dd117defb06f780cf6b2ea89ce4f15a34a95337e
Akashdip Mahapatra <81384987+akashdip2001@users.noreply.github.com>
2024-04-05 00:39:35 +0530

    Update onlyADD.c

--- c_project/onlyADD.c
+++ c_project/onlyADD.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-struct record{
+struct record {
     char date[12];
     int id; 
     char name[10];
@@ -11,9 +11,14 @@ struct record{
     int percentage;
 };
 
-void add(){
+void clearBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void add() {
     FILE *fp;
-    fp = fopen("Fileprac.txt", "ab");
+    fp = fopen("Fileprac.txt", "wt"); // Change mode to write text "ab" to "wt"
     if (fp == NULL) {
         printf("Error opening file.\n");
         return;
@@ -28,19 +33,48 @@ void add(){
 
     printf("Enter the id: ");
     scanf("%d", &st.id);
+    clearBuffer(); // Clear the input buffer (Previous text from memory)
 
     printf("Enter the name: ");
-    scanf("%9s", st.name);
+    scanf("%9[^\n]", st.name);
+    clearBuffer(); // same
 
     printf("Enter the total: ");
     scanf("%u", &st.total);
-    
+    clearBuffer(); // same
+
+    // Calculate percentage
+    st.percentage = (int)((st.total / 1000.0) * 100); // calculation
+
     printf("Added successfully\n");
-    fwrite(&st, sizeof(st), 1, fp);
+    fprintf(fp, "%s\n %d\n %s\n %u\n %d\n", st.date, st.id, st.name, st.total, st.percentage); // text in Notepad 
     fclose(fp);
 }
 
-int main(){
+void printRecords() {
+    FILE *fp;
+    fp = fopen("Fileprac.txt", "rt"); // Change mode to read text mode - E=mc2
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    struct record st;
+
+    while (fscanf(fp, "%s %d %s %u %d", st.date, &st.id, st.name, &st.total, &st.percentage) != EOF) { // text in Terminal - E=mc2
+        printf("Date: %s\n", st.date);
+        printf("ID: %d\n", st.id);
+        printf("Name: %s\n", st.name);
+        printf("Total: %u\n", st.total);
+        printf("Percentage: %d\n", st.percentage);
+        printf("\n");
+    }
+
+    fclose(fp);
+}
+
+int main() {
     add();
+    printRecords();
     return 0;
 }
